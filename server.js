2a66fa5a247cd284c1c2946558e9ffad07ed1417
Adrián López <63605184+Voltriixz@users.noreply.github.com>
2023-04-02 16:32:58 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+app.get("/sw.js", function (request, response) {
+  response.set('Cache-Control', 'no-cache');
+  response.sendFile(__dirname + '/public/sw.js');
+});
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  if (!request.get('X-Forwarded-Proto').includes('https')) {
+    response.redirect(301, 'https://voltriixz.github.io/F1-Start-Test');
+    return;
+  }
+  response.set('Cache-Control', 'no-cache');
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
